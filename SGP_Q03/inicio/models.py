f41924a3cf12cedb240dab87e12d7621da90dfbb
mabpg <mabpen195@gmail.com>
2014-04-12 00:37:33 -0400

    Se agrega el test para login

---
+++ SGP_Q03/inicio/models.py
@@ -0,0 +1,11 @@
+# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Perfiles(models.Model):
+    usuario = models.OneToOneField(User)
+    telefono = models.IntegerField()
+
+    def __unicode__(self):
+        return self.usuario.username
