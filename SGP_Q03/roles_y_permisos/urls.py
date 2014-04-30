c8e1a1b4b19a00f332538b8eb9b8f28a1a3ad646
alforro <alfa.alvaro.rodriguez@gmail.com>
2014-04-30 19:36:46 -0400

    arreglando problemas

--- SGP_Q03/roles_y_permisos/urls.py
+++ SGP_Q03/roles_y_permisos/urls.py
@@ -6,8 +6,5 @@ urlpatterns = patterns('',
   url(r'^(?P<pk>\d+)$', rol_list, name='lista_rol'),
   url(r'^nuevo$', rol_create, name='nuevo_rol'),
   url(r'^editar/(?P<pk>\d+)$', rol_update, name='editar_rol'),
-<<<<<<< HEAD
-=======
-  #url(r'^borrar/(?P<pk>\d+)$', fase_delete, name='borrar_fase'),
->>>>>>> 136eae0af3cccbe5b25a9b34cc142a4795ccddd8
+
 )
--- SGP_Q03/roles_y_permisos/views.py
+++ SGP_Q03/roles_y_permisos/views.py
@@ -45,15 +45,3 @@ def  rol_update(request, pk, template_name='roles/rol_form.html'):
         form.save()
         return redirect('lista_fase')
     return render(request, template_name, {'form':form})
-
-@login_required
-<<<<<<< HEAD
-def proyecto_delete(request, pk, template_name='roles/rol_confirm_delete.html'):
-=======
-def rol_delete(request, pk, template_name='roles/rol_confirm_delete.html'):
->>>>>>> 136eae0af3cccbe5b25a9b34cc142a4795ccddd8
-    server = get_object_or_404(Rol, pk=pk)
-    if request.method=='POST':
-        server.delete()
-        return redirect('lista_fase')
-    return render(request, template_name, {'object':server})
