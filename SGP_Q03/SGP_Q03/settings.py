42fab6c2014255323fb5f97de27b417b8feccec2
mabpg <mabpen195@gmail.com>
2014-04-30 21:34:40 -0400

    Se modificaron fase_list y proyecto_list

--- SGP_Q03/SGP_Q03/settings.py
+++ SGP_Q03/SGP_Q03/settings.py
@@ -74,7 +74,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'postgres',
-        'USER': 'postgres',
+        'USER': 'postgresql',
 	    'PASSWORD': 'postgres',
         'HOST': 'localhost',
         'PORT':'5432',
