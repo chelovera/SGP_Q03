e06e486b197b3f386fe2fbaeb001053903bd66a9
mabpg <mabpen195@gmail.com>
2014-05-02 15:10:55 -0400

    se modificaron proyecto_list, fases_list, usuario_list y se agregaron comentarios para la documentacion en los models

--- SGP_Q03/proyectos/models.py
+++ SGP_Q03/proyectos/models.py
@@ -3,7 +3,49 @@ from django.core.urlresolvers import reverse
 from usuarios.models import Usuario
 
 
+"""
+Django proyectos/models
+
+"""
+
 class Proyecto(models.Model):
+
+    """
+        Estos son los campos que contiene proyectos/models.py
+
+        @param codigo: Es el primary key, se genera automaticamente
+        @type codigo: models.AutoField(primary_key= True)
+
+        @param nombre: Es el nombre del proyecto
+        @type nombre: models.CharField(max_length=50)
+
+        @param descripcion: en este campo se indican las caracteristicas mas resaltantes del proyecto
+        @type descripcion : models.CharField(max_length=200)
+
+        @param estado: indica que el proyecto puede estar en estado Pendiente o Finalizado
+        @type estado: models.CharField(max_length=10, choices=ESTADOS, default='Pendiente')
+
+        @param fecha_ini: es la fecha de inicio del proyecto
+        @type fecha_ini: models.DateField(null=True)
+
+        @param fecha_fin:  es la fecha de finalizacion del proyecto
+        @type fecha_fin : models.DateField(null=True)
+
+        @param costo_temporal: es el tiempo estimativo que llevara realizar el proyecto
+        @type costo_temporal : models.PositiveIntegerField(default=0, null=True)
+
+        @param costo_monetario : son los recursos monetarios estimativos a utilizarse para realizar el proyecto
+        @type costo_monetario : models.PositiveIntegerField(null=True)
+
+        @param lider : se establece el lider del proyecto
+        @type lider : models.ForeignKey(Usuario)
+
+
+
+    """
+
+
+
     #estos son estados de prueba
     ESTADOS = (
         ('Pendiente', 'pendiente'),
--- SGP_Q03/usuarios/models.py
+++ SGP_Q03/usuarios/models.py
@@ -2,7 +2,39 @@ from django.contrib.auth.models import User
 from django.db import models
 from django.core.urlresolvers import reverse
 
+"""
+Django usuarios/models
+
+"""
+
 class Usuario(User):
+
+    """
+
+     Estos son los campos que contiene usuarios/models.py
+
+
+        @param nombre : Es el nombre del usuario, es un campo obligatorio
+        @type nombre : models.CharField(max_length=50, null=False)
+
+        @param apellido : Es el nombre del usuario, es un campo obligatorio
+        @type apellido : models.CharField(max_length=50, null=False)
+
+        @param telefono : es el telefono de contacto del usuario
+        @type telefono : models.PositiveIntegerField(default=0)
+
+        @param cedula : es el documento de identidad del usuario
+        @type cedula : models.PositiveIntegerField(default=0)
+
+        @param direccion: en este campo se indica la direccion del usuario
+        @type direccion : models.CharField(max_length=50, null=False, blank=True)
+
+        @param estado: indica que el usuario puede estar en estado Activo o Inactivo
+        @type estado : models.BooleanField(default=True)
+
+    """
+
+
     nombre = models.CharField(max_length=50, null=False)
     apellido = models.CharField(max_length=50, null=False)
     telefono = models.PositiveIntegerField(default=0)
