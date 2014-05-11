33f471f015dea6bf5ee85c06f948ce841a2ea05a
alforro <alfa.alvaro.rodriguez@gmail.com>
2014-05-11 18:32:52 -0400

    script para poblar base de datos

--- SGP_Q03/cargar_db.py
+++ SGP_Q03/cargar_db.py
@@ -1,7 +1,54 @@
 from usuarios.models import Usuario
 from proyectos.models import Proyecto
-
+from fases.models import Fase
 
 #Proyecto.objects.all().delete()
+#ejecutar asi ./manage.py shell < cargar_db.py
 Usuario.objects.all().delete()
-u = Usuario.objects.create_user(username='alvarito',nombre='Alvaro',apellido='Rodriguez',telefono='0961940704',cedula='4617510',direccion='Calle Rio Negro esq. Rio Jejui nro 315', email='test@test.com',password='alvarito')
+Fase.objects.all().delete()
+Proyecto.objects.all().delete()
+usuario1 = Usuario.objects.create_user(username='alvaro_user', nombre='Alvaro', apellido='Rodriguez',
+                                       telefono='0961940704', cedula='4617510',
+                                       direccion='Calle Rio Negro esq. Rio Jejui 315',
+                                       email='alfa.alvaro.rodriguez@gmail.com', password='alvaro_user')
+usuario2 = Usuario.objects.create_user(username='homero', nombre='Homero', apellido='Simpson', telefono='5556832',
+                                       cedula='123467', direccion='Avenida Siempreviva 742',
+                                       email='amantedelacomida53@aol.com', password='homero')
+usuario3 = Usuario.objects.create_user(username='walter', nombre='Walter', apellido='White', telefono='5551234',
+                                       cedula='8910111', direccion='308 de Negra Arroyo Lane', email='walter@gmail.com',
+                                       password='walter')
+usuario4 = Usuario.objects.create_user(username='john', nombre='John', apellido='Snow', telefono='5555678',
+                                       cedula='2131415', direccion='en el muro', email='john@gmail.com',
+                                       password='john')
+usuario5 = Usuario.objects.create_user(username='bruce', nombre='Bruce', apellido='Banner', telefono='5559012',
+                                       cedula='1617181', direccion='ni idea', email='banner@gmail.com',
+                                       password='banner')
+
+proyecto1 = Proyecto(nombre='alpha project', descripcion='este proyecto corresponde a Alvaro Rodriguez',
+                     costo_temporal=10, costo_monetario=10, lider=usuario1)
+proyecto2 = Proyecto(nombre='beta project', descripcion='este proyecto corresponde a Homero Simpson', costo_temporal=10,
+                     costo_monetario=10, lider=usuario2)
+proyecto3 = Proyecto(nombre='gamma project', descripcion='este proyecto corresponde a Walter White', costo_temporal=10,
+                     costo_monetario=10, lider=usuario3)
+proyecto4 = Proyecto(nombre='delta project', descripcion='este proyecto corresponde a John Snow', costo_temporal=10,
+                     costo_monetario=10, lider=usuario4)
+proyecto5 = Proyecto(nombre='epsilon project', descripcion='este proyecto corresponde a Bruce Banner',
+                     costo_temporal=10, costo_monetario=10, lider=usuario5)
+
+proyecto1.save()
+proyecto2.save()
+proyecto3.save()
+proyecto4.save()
+proyecto5.save()
+
+fase1 = Fase(nombre='primera fase', descripcion='primera fase ', proyecto=proyecto1)
+fase2 = Fase(nombre='segunda fase', descripcion='segunda fase', proyecto=proyecto1)
+fase3 = Fase(nombre='tercera fase', descripcion='tercera fase', proyecto=proyecto1)
+fase4 = Fase(nombre='cuarta fase', descripcion='cuarta fase', proyecto=proyecto1)
+fase5 = Fase(nombre='quinta fase', descripcion='quinta fase', proyecto=proyecto1)
+
+fase1.save()
+fase2.save()
+fase3.save()
+fase4.save()
+fase5.save()
